95ebebc4c2a89f260ab78c4c9cc25059863dcd6f
RedBoardDev <thomas.ott@epitech.eu>
2025-12-17 15:43:04 +0100

    Refactor project structure and enhance core functionality with additional features.

---
+++ oracle/src/Oracle.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Oracle {
+    address public owner;
+    address[] public nodes;
+    mapping(address => bool) public isNode;
+    mapping(string => uint256) public currentPrices;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getQuorum() public view returns (uint256) {
+        uint256 nodeCount = nodes.length;
+        if (nodeCount < 3) {
+            return 3;
+        }
+        return (nodeCount * 2 + 2) / 3;
+    }
+
+    function addNode() public {
+        require(!isNode[msg.sender], "Node already exists");
+        isNode[msg.sender] = true;
+        nodes.push(msg.sender);
+    }
+
+    function removeNode() public {
+        require(isNode[msg.sender], "Node does not exist");
+        isNode[msg.sender] = false;
+
+        uint256 nodeCount = nodes.length;
+        for (uint256 i = 0; i < nodeCount; i++) {
+            if (nodes[i] == msg.sender) {
+                nodes[i] = nodes[nodeCount - 1];
+                nodes.pop();
+                break;
+            }
+        }
+    }
+}
